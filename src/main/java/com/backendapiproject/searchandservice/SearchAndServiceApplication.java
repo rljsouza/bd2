fa5bb3e4a6ac749a0fcec34aa2ba86f46ef4d312
Ricardo Luiz Jordão de Souza <ricardoluizjordao.rlj@gmail.com>
2024-04-14 19:12:13 -0300

    create project

---
+++ src/main/java/com/backendapiproject/searchandservice/SearchAndServiceApplication.java
@@ -0,0 +1,13 @@
+package com.backendapiproject.searchandservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchAndServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchAndServiceApplication.class, args);
+	}
+
+}
